a0fbac24b0c7a9b2b63cf1bf2169c4d5a6a9e6ff
mallemmallikarjuna <40300768+mallemmallikarjuna@users.noreply.github.com>
2021-03-28 18:08:00 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,6 +5,6 @@ resource "aws_instance" "myec2" {
   ami           = "ami-06f3207f56dc1ca18" # us-west-2
   instance_type = "t2.micro"
   tags = {
-    "Name" = "mallik-pratice"
+    "Name" = "arya-pratice"
   }
 }
