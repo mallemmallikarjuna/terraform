2d552ee578567d4f707ce751ebbdcdbb1c36e09e
mallemmallikarjuna <40300768+mallemmallikarjuna@users.noreply.github.com>
2021-03-28 17:19:10 +0530

    Update ec2.tf
    file updated

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,12 @@
 provider "aws" {
 region = "ap-northeast-2"
+  shared_credentials_file = "/root/.aws/credentials"
+
 }
 resource "aws_instance" "myec2" {
   ami           = "ami-06f3207f56dc1ca18" # us-west-2
   instance_type = "t2.micro"
   tags = {
-    "Name" = "naveen-pratice"
+    "Name" = "arjun-pratice"
   }
 }
