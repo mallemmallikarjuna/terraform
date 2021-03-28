3699bf2b1d9d8168b539e8e4912988062ff13e3a
mallemmallikarjuna <40300768+mallemmallikarjuna@users.noreply.github.com>
2021-03-28 17:50:22 +0530

    Update ec2.tf
    removed extra lines

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,6 @@
 provider "aws" {
 region = "ap-northeast-2"
-  shared_credentials_file = "/root/.aws/credentials"
-  profile = "default"
-}
+ }
 resource "aws_instance" "myec2" {
   ami           = "ami-06f3207f56dc1ca18" # us-west-2
   instance_type = "t2.micro"
