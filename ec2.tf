87f7029fb575c2f4dc9f1222417638653111ea32
mallemmallikarjuna <40300768+mallemmallikarjuna@users.noreply.github.com>
2021-03-29 11:38:27 +0530

    Update ec2.tf
    new security group added

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,32 @@
 provider "aws" {
 region = "ap-northeast-2"
  }
+resource "aws_security_group" "morning-ssh-http" {
+  name        = "morning-ssh-http"
+  description = "allow ssh and http traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
 resource "aws_instance" "myec2" {
   ami           = "ami-06f3207f56dc1ca18" # us-west-2
   instance_type = "t2.micro"
