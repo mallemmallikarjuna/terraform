823e0026c5a6db0a9e82e3ec8bd8b209b4f1f9a2
mallemmallikarjuna <40300768+mallemmallikarjuna@users.noreply.github.com>
2021-03-29 11:27:02 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,16 @@
 provider "aws" {
 region = "ap-northeast-2"
  }
+ resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.web.id
+}
 resource "aws_instance" "myec2" {
   ami           = "ami-06f3207f56dc1ca18" # us-west-2
   instance_type = "t2.micro"
   tags = {
     "Name" = "mallik-pratice"
   }
- resource "aws_volume_attachment" "ebs_att" {
-  device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.example.id
-  instance_id = aws_instance.web.id
-}
+
 }
