e9af70f0200b954d094c18ae6fb3ece519316956
mallemmallikarjuna <40300768+mallemmallikarjuna@users.noreply.github.com>
2021-03-29 11:32:32 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,21 @@
 provider "aws" {
 region = "ap-northeast-2"
  }
- resource "aws_volume_attachment" "ebs_att" {
-  device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.example.id
-  instance_id = aws_instance.web.id
+ variable "device_name" {default=["/dev/sdd"]}
+variable "ec2_ebs_volume_count" {default=1}
+resource "aws_ebs_volume" "ebs_volume" {
+  availability_zone = aws_instance.myin2.availability_zone
+  size = 2
+  tags = {
+    Name = "myebs1"
+  }
 }
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdd"
+  volume_id = aws_ebs_volume.ebs_volume.id
+  instance_id = aws_instance.myin2.id
+  force_detach = true
+} 
 resource "aws_instance" "myec2" {
   ami           = "ami-06f3207f56dc1ca18" # us-west-2
   instance_type = "t2.micro"
