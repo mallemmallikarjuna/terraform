b63e4e59550393db3960b57ee0ab3ec609cdc164
mallemmallikarjuna <40300768+mallemmallikarjuna@users.noreply.github.com>
2021-03-29 11:59:25 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -30,6 +30,7 @@ resource "aws_security_group" "morning-ssh-http" {
 resource "aws_instance" "myec2" {
   ami           = "ami-06f3207f56dc1ca18" # us-west-2
   instance_type = "t2.micro"
+  security_groups=[ aws_security_group.morning-ssh-http.id ]
   tags = {
     "Name" = "mallik-pratice"
   }
